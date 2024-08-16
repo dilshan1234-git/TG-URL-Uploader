ad8a23262b1a3792e8885df51b2be2e233ed630c
dilshan1234-git <112808062+dilshan1234-git@users.noreply.github.com>
2024-08-16 11:11:06 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -30,48 +30,44 @@ def GetExpiryDate(chat_id):
 
 
 @pyrogram.Client.on_message(pyrogram.filters.command(["help", "about"]))
-async def help_user(bot, update):
-    # logger.info(update)
+async def help_user(bot, message):
+    # logger.info(message)
     await bot.send_message(
-        chat_id=update.chat.id,
+        chat_id=message.chat.id,
         text=Translation.HELP_USER,
         parse_mode="html",
-        disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
+        disable_web_page_preview=True
     )
 
 
 @pyrogram.Client.on_message(pyrogram.filters.command(["me"]))
-async def get_me_info(bot, update):
-    # logger.info(update)
-    chat_id = str(update.from_user.id)
+async def get_me_info(bot, message):
+    # logger.info(message)
+    chat_id = str(message.from_user.id)
     chat_id, plan_type, expires_at = GetExpiryDate(chat_id)
     await bot.send_message(
-        chat_id=update.chat.id,
+        chat_id=message.chat.id,
         text=Translation.CURENT_PLAN_DETAILS.format(chat_id, plan_type, expires_at),
         parse_mode="html",
-        disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
+        disable_web_page_preview=True
     )
 
 
 @pyrogram.Client.on_message(pyrogram.filters.command(["start"]))
-async def start(bot, update):
-    # logger.info(update)
+async def start(bot, message):
+    # logger.info(message)
     await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.START_TEXT,
-        reply_to_message_id=update.message_id
+        chat_id=message.chat.id,
+        text=Translation.START_TEXT
     )
 
 
 @pyrogram.Client.on_message(pyrogram.filters.command(["upgrade"]))
-async def upgrade(bot, update):
-    # logger.info(update)
+async def upgrade(bot, message):
+    # logger.info(message)
     await bot.send_message(
-        chat_id=update.chat.id,
+        chat_id=message.chat.id,
         text=Translation.UPGRADE_TEXT,
         parse_mode="html",
-        reply_to_message_id=update.message_id,
         disable_web_page_preview=True
     )
